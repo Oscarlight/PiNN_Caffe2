f1f6d38ea50a0bfbef50d5ef1ca73e6401a3f1f6
FrankLeeX <330952489@qq.com>
2017-09-06 11:31:20 -0400

    Modified parser.py by Xiang Li

--- parser.py
+++ parser.py
@@ -2,78 +2,121 @@
 # HEMT_Bo folder to numpy arrays of features and labels
 
 def parse_mdm_to_nparray(file_name):
+
 	with open(file_name,'r') as f:
 
-    		  tmp = ''
-    		  tmp = f.readline().split()
+		tmp = ''
+		tmp = f.readline().split()
 
-  		  # Ignore the uesless lines
-  		  if len(tmp) == 0 or tmp[0] =='!':
- 		       tmp = f.readline().split()
+		# Ignore the uesless lines
+		if len(tmp) == 0 or tmp[0] =='!':
+			tmp = f.readline().split()
 
- 		  # Read header (including inputs and outputs)
-  		  header = []
-  		  inputs = []
-  		  outputs = []
-  		  if (tmp[0] == "BEGIN_HEADER"):
-   		     tmp = f.readline().split()
-   		     while (tmp[0] != "END_HEADER"):
-   		         header.append(tmp)
-   		         tmp = f.readline().split()
-		    print('header',header)
+		# Read header (including inputs and outputs)
+		header = []
+		inputs = []
+		outputs = []
+		if (tmp[0] == "BEGIN_HEADER"):
+			tmp = f.readline().split()
+			while (tmp[0] != "END_HEADER"):
+				header.append(tmp)
+				tmp = f.readline().split()
 
- 		  # Separate inputs and outputs
-  		  i = 0
-   		  if (header[i][0] == 'ICCAP_INPUTS'):
-      		  while (header[i][0] != 'ICCAP_OUTPUTS'):
-           		 i = i + 1
-           		 inputs.append(header[i-1])
-   		  if (header[i][0] == 'ICCAP_OUTPUTS'):
-      		  while (i < len(header)):
-        		 outputs.append(header[i])
-        		 i = i+1
-   		  print('inputs:', inputs)
-    		  print('outputs:', outputs)
+		# Separate inputs and outputs
+		i = 0
+		if (header[i][0] == 'ICCAP_INPUTS'):
+			while (header[i][0] != 'ICCAP_OUTPUTS'):
+				i = i + 1
+				inputs.append(header[i-1])
+		if (header[i][0] == 'ICCAP_OUTPUTS'):
+			while (i < len(header)):
+			outputs.append(header[i])
+			i = i+1
 
-   		  # Read Data
-   		  Condition = []
-   		  Data = []
+		# Read Data
+		Condition = []
+		Data_tmp = []
 
-   		  while (1):
-      		 	tmp = f.readline().split()
-        	  	# Ignore the uesless lines
-        		if len(tmp) == 0 or tmp[0] =='!':
-       				tmp = f.readline().split()
-           		if len(tmp) == 0:
-       		        break
+		while (1):
+			tmp = f.readline().split()
+			# Ignore the uesless lines
+			if len(tmp) == 0 or tmp[0] =='!':
+				tmp = f.readline().split()
+				if len(tmp) == 0:
+					break
+			cond = []
+			data = []
+			if (tmp[0] == "BEGIN_DB"):
+				tmp = f.readline().split()
+				while (tmp[0] == "ICCAP_VAR"):
+					cond.append(tmp)
+					tmp = f.readline().split()
+					if len(tmp) ==0:
+						tmp = f.readline().split()
+				while (tmp[0] != "END_DB"):
+					data.append(tmp)
+					tmp = f.readline().split()
 
-      		  cond = []
-       		  data = []
+			# Transfer string to float
+			j = 1
+			while (j < len(data)):
+				k = 0
+				while (k < len(data[0])):
+					data[j][k] = float(data[j][k])
+					k = k+1
+				j = j+1
 
-        	  if (tmp[0] == "BEGIN_DB"):
-            		tmp = f.readline().split()
-            		while (tmp[0] == "ICCAP_VAR"):
-                		cond.append(tmp)
-                		tmp = f.readline().split()
-                		if len(tmp) ==0:
-                   			tmp = f.readline().split()
-           		while (tmp[0] != "END_DB"):
-                		data.append(tmp)
-                		tmp = f.readline().split()
-       		  print('cond:', cond)
+			Condition.append(cond)
+			Data_tmp.append(data)
 
-       		  # Transfer string to float
-       		  j = 1
-        	  while (j < len(data)):
-           		k = 0
-            		while (k < len(data[0])):
-               			data[j][k] = float(data[j][k])
-                		k = k+1
-            		j = j+1
-        	  print('data:', data)
+		# Construct Data dictionary
+		Data = {}
 
-        	  Condition.append(cond)
-        	  Data.append(data)
+		cond_val_num = len(Condition[0])
+		cond_num = len(Condition)
+		data_val_num = len(Data_tmp[0][0])
+		data_num = len(Data_tmp[0])-1
 
-    		  print('Condition:',Condition)
-    		  print('Data:',Data)
+		store_tmp = []
+		data_val_count = 0
+		cond_val_count = 0
+		# Data
+		while (data_val_count < data_val_num):
+			data_store_tmp = []
+			cond_count = 0
+			while (cond_count < cond_num):
+				data_count = 0
+				while (data_count < data_num):
+					data_store_tmp.append(Data_tmp[cond_count][data_count+1][data_val_count])
+					data_count = data_count+1
+				cond_count = cond_count+1
+			data_val_count = data_val_count+1
+			store_tmp.append(data_store_tmp)
+		# Condition
+		while (cond_val_count < cond_val_num):
+			cond_store_tmp = []
+        		cond_count = 0
+			while (cond_count < cond_num):
+				data_count = 0
+				while (data_count < data_num):
+					cond_store_tmp.append(float(Condition[cond_count][cond_val_count][2]))
+					data_count = data_count+1
+				cond_count = cond_count+1
+			cond_val_count = cond_val_count+1
+			store_tmp.append(cond_store_tmp)
+
+		# Combine together
+		count = 0
+		while (count < data_val_num):
+			Data[Data_tmp[0][0][count]] = store_tmp[count]
+			count = count+1
+		while (count < data_val_num+cond_val_num):
+			Data[Condition[0][count-data_val_num][1]] = store_tmp[count]
+			count = count+1
+
+		# Construct Header dictionary
+		Header = {}
+		Header['Inputs'] = inputs
+		Header['Outputs'] = outputs
+
+		return Header, Data
