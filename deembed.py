317bc144a6316e6ab320f689b423e92c415c7751
Oscarlight <oscarli2716@gmail.com>
2017-09-08 14:34:08 -0400

    Task#1

--- deembed.py
+++ deembed.py
@@ -1,4 +1,10 @@
 # read in S parameter, bias and frequency infromation,
-# deembed parasitic resistors and inductors, then output deembeded Y parameters 
+# deembed parasitic resistors and inductors, then output deembeded Y parameters
+# Reference: 
+#  Deembedding method:
+#  	http://ieeexplore.ieee.org/stamp/stamp.jsp?arnumber=3650
+#  How deembedding fit into the ANN device modeling:
+#    http://www.keysight.com/upload/cmc_upload/All/NeuroFET_Webcast_Final.pdf?&cc=US&lc=eng
+
 def deembed():
 	pass
