d002e90c787c3deecfe27e2437dfe6d6d0eaa5dc
FrankLeeX <330952489@qq.com>
2017-09-16 15:50:31 -0400

    Finish the plot functions (4 in total) in visualizer.py and test_visualizer

--- visualizer.py
+++ visualizer.py
@@ -1,6 +1,141 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 def write_csv_file (file_name, data, description):
-    '''' Write data to .csv file'''
+    ''' Write data to .csv file.'''
     np.savetxt(file_name, data, delimiter=',', newline='\n', header=description)
 
+
+def plot_linear_Id_vs_Vd_at_Vg (header, vg, vd, id):
+
+    vg_count = 0
+    vd_count = 0
+    for item in header['Inputs']:
+        if (item[0] == 'vd'):
+            vd_count = int(item[10])
+        elif (item[0] == 'vg'):
+            vg_count = int(item[10])
+        else:
+            pass
+
+    i = 0
+    while (i < vg_count):
+        vd_tmp = vd[i*vd_count : i*vd_count+vd_count]
+        id_tmp = id[i*vd_count : i*vd_count+vd_count]
+        vg_label = 'vg='+str(vg[i*vd_count])
+        plt.plot(vd_tmp, id_tmp, label = vg_label)
+        i = i+1
+
+    plt.xlabel('vd')
+    plt.ylabel('id')
+    plt.legend(loc = 'upper left')
+    plt.show()
+
+def plot_linear_Id_vs_Vg_at_Vd (header, vg, vd, id):
+
+    vg_count = 0
+    vd_count = 0
+    for item in header['Inputs']:
+        if (item[0] == 'vd'):
+            vd_count = int(item[10])
+        elif (item[0] == 'vg'):
+            vg_count = int(item[10])
+        else:
+            pass
+
+    # Re-arrange the data so that data is sorted by vd instead of vg
+    sort_vg = []
+    sort_vd = []
+    sort_id = []
+    i = 0
+    while (i < vd_count):
+        j = 0
+        while (j < vg_count):
+            sort_vg.append(vg[j*vd_count+i])
+            sort_vd.append(vd[j*vd_count+i])
+            sort_id.append(id[j*vd_count+i])
+            j = j+1
+        i = i+1
+
+    k = 0
+    while (k < vd_count):
+        vg_tmp = sort_vg[k*vg_count : k*vg_count+vg_count]
+        id_tmp = sort_id[k*vg_count : k*vg_count+vg_count]
+        vd_label = 'vd='+str(sort_vd[k*vg_count])
+        plt.plot(vg_tmp, id_tmp, label = vd_label)
+        k = k+1
+
+    plt.xlabel('vg')
+    plt.ylabel('id')
+    plt.legend(loc = 'upper left', ncol = 3)
+    plt.show()
+
+def plot_log_Id_vs_Vd_at_Vg (header, vg, vd, id):
+
+    vg_count = 0
+    vd_count = 0
+    for item in header['Inputs']:
+        if (item[0] == 'vd'):
+            vd_count = int(item[10])
+        elif (item[0] == 'vg'):
+            vg_count = int(item[10])
+        else:
+            pass
+
+    i = 0
+    while (i < vg_count):
+        vd_tmp = vd[i*vd_count : i*vd_count+vd_count]
+        id_tmp = np.abs(id[i*vd_count : i*vd_count+vd_count])
+        vg_label = 'vg='+str(vg[i*vd_count])
+        print(vd_tmp)
+        print(len(vd_tmp))
+        print(id_tmp)
+        print(len(id_tmp))
+        plt.plot(vd_tmp, id_tmp, label = vg_label)
+        i = i+1
+
+    plt.xlabel('vd')
+    plt.ylabel('id')
+    plt.yscale('log')
+    plt.legend(loc = 'lower right')
+    plt.show()
+
+def plot_log_Id_vs_Vg_at_Vd (header, vg, vd, id):
+
+    vg_count = 0
+    vd_count = 0
+    for item in header['Inputs']:
+        if (item[0] == 'vd'):
+            vd_count = int(item[10])
+        elif (item[0] == 'vg'):
+            vg_count = int(item[10])
+        else:
+            pass
+
+    # Re-arrange the data so that data is sorted by vd instead of vg
+    sort_vg = []
+    sort_vd = []
+    sort_id = []
+    i = 0
+    while (i < vd_count):
+        j = 0
+        while (j < vg_count):
+            sort_vg.append(vg[j*vd_count+i])
+            sort_vd.append(vd[j*vd_count+i])
+            sort_id.append(id[j*vd_count+i])
+            j = j+1
+        i = i+1
+
+    k = 0
+    while (k < vd_count):
+        vg_tmp = sort_vg[k*vg_count : k*vg_count+vg_count]
+        id_tmp = np.abs(sort_id[k*vg_count : k*vg_count+vg_count])
+        vd_label = 'vd='+str(sort_vd[k*vg_count])
+        plt.plot(vg_tmp, id_tmp, label = vd_label)
+        k = k+1
+
+    plt.xlabel('vg')
+    plt.ylabel('id')
+    plt.yscale('log')
+    plt.legend(loc = 'lower right', ncol = 3)
+    plt.show()
--- visualizer_test.py
+++ visualizer_test.py
@@ -9,13 +9,16 @@ import visualizer
 
 class TestVisualizer(unittest.TestCase):
 	def test_visualizer(self):
+		header, data = parser.parse_mdm_to_nparray("./HEMT_bo/Id_vs_Vd_at_Vg.mdm")
 		vg, vd, ids = parser.read_dc_iv_mdm("./HEMT_bo/Id_vs_Vd_at_Vg.mdm")
 		scale, vg_shift = preproc.compute_dc_meta(vg, vd, ids)
 		preproc_vg, preproc_vd, preproc_id, restore_func = preproc.dc_iv_preproc(
 			vg, vd, ids, scale, vg_shift
 		)
 		data_out = np.array([preproc_vg, preproc_vd, preproc_id])
-		write_csv_file('after_pre-process.csv', data_out, 'Row 1: Vg, Row 2: Vd, Row 3: Id')
+		visualizer.write_csv_file('after_pre-process.csv', data_out, 'Row 1: Vg, Row 2: Vd, Row 3: Id')
+		visualizer.plot_linear_Id_vs_Vd_at_Vg (header, vg, vd, id)
+		visualizer.plot_log_Id_vs_Vg_at_Vd (header, vg, vd, id)
 
 
 
