7e552db332d2fa9167a7bd1515e81a485128a590
Malavika <mpa46@cornell.edu>
2017-10-03 18:33:21 -0400

    ac preproc

--- pinn/preproc.py
+++ pinn/preproc.py
@@ -71,4 +71,40 @@ def truncate(data_arrays, truncate_range, axis):
 
     return [e[index] for e in data_arrays]
 
+#AC QV preproc
+def ac_qv_preproc(vg, vd, gradient, scale, shift):
+    preproc_vg = (vg-shift) / scale['vg']
+    preproc_vd = vd / scale['vd']
+    preproc_gradient = gradient/scale['q']
+    return preproc_vg, preproc_vd, preproc_gradient
+
+def get_restore_q_func(
+        scale, shift
+):
+        def restore_integral_func(integrals):
+            ori_integral = integrals*scale['q']#* scale['vd']*scale['vg']
+            return ori_integral
+        def restore_gradient_func(gradient):
+                ori_gradient =  gradient * scale['q']
+                return ori_gradient
+        return restore_integral_func, restore_gradient_func
+    
+def compute_ac_meta(vg, vd, gradient, checkaccuracy = False):
+    vg_shift = np.median(vg)-0.0
+    vg_scale = max(abs(np.max(vg)-vg_shift)/1.0, abs(np.min(vg)-vg_shift)/1.0)
+    vd_scale = max(abs(np.max(vd))/1.0, abs(np.min(vd))/1.0)
+    q_scale = max(abs(np.max(gradient))/0.75, abs(np.min(gradient))/0.75)   
+    scale = {'vg':vg_scale, 'vd':vd_scale, 'q':q_scale}
+    return scale, vg_shift
+
+
+def plotgradient(vg, vd, gradient):
+    gradient = np.asarray(gradient)
+    dqdvg = gradient[:, 0]
+    dqdvd = gradient[:, 1]
+    plt.plot(vg, dqdvg, 'r')
+    plt.plot(vd, dqdvd, 'b')
+    plt.show()
+
+  
 
