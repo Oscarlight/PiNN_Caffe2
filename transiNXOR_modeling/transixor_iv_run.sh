2eed81efa4a4108915c7d20ffc1d1e0c0bd014d0
OscarLight <oscarli2716@gmail.com>
2018-01-25 01:31:57 -0500

    remove zero-label example in training

--- transiNXOR_modeling/transixor_iv_run.sh
+++ transiNXOR_modeling/transixor_iv_run.sh
@@ -1,9 +1,9 @@
-python transixor_trainer.py transiXOR_Models/bise_h18 -hidden 8 -layer 1
-python transixor_trainer.py transiXOR_Models/bise_h28 -hidden 8 -layer 2
-python transixor_trainer.py transiXOR_Models/bise_h116 -hidden 16 -layer 1
-python transixor_trainer.py transiXOR_Models/bise_h216 -hidden 16 -layer 2
+python transixor_trainer.py transiXOR_Models/bise_h18_0 -hidden 8 -layer 1
+python transixor_trainer.py transiXOR_Models/bise_h28_0 -hidden 8 -layer 2
+python transixor_trainer.py transiXOR_Models/bise_h116_0 -hidden 16 -layer 1
+python transixor_trainer.py transiXOR_Models/bise_h216_0 -hidden 16 -layer 2
 
-python transixor_trainer.py transiXOR_Models/bise_h18 -hidden 8 -layer 1 -mls 500
-python transixor_trainer.py transiXOR_Models/bise_h28 -hidden 8 -layer 2 -mls 500
-python transixor_trainer.py transiXOR_Models/bise_h116 -hidden 16 -layer 1 -mls 500
-python transixor_trainer.py transiXOR_Models/bise_h216 -hidden 16 -layer 2 -mls 500
+python transixor_trainer.py transiXOR_Models/bise_h18_1 -hidden 8 -layer 1 -mls 500
+python transixor_trainer.py transiXOR_Models/bise_h28_1 -hidden 8 -layer 2 -mls 500
+python transixor_trainer.py transiXOR_Models/bise_h116_1 -hidden 16 -layer 1 -mls 500
+python transixor_trainer.py transiXOR_Models/bise_h216_1 -hidden 16 -layer 2 -mls 500
