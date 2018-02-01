e56ae674cf50fb442d5c40794ad065a90be39bbe
OscarLight <oscarli2716@gmail.com>
2018-02-01 08:26:32 -0500

    c model for transiXNOR

--- transiNXOR_modeling/c_model/main.c
+++ transiNXOR_modeling/c_model/main.c
@@ -3,6 +3,12 @@
 #include "device_model.h"
 
 int main(int argc, char** argv) {
-	float id = device_model(0.0, 0.0, 0.2, 1);
+	float id = device_model(0.2, 0.2, 0.2, 1);
+	printf("%f\n", id);
+	id = device_model(0.0, 0.0, 0.2, 1);
+	printf("%f\n", id);
+	id = device_model(0.1, 0.0, 0.2, 1);
+	printf("%f\n", id);
+	id = device_model(0.0, 0.1, 0.2, 1);
 	printf("%f\n", id);
 }
