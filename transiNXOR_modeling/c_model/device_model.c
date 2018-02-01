06c6a4970a38cccd18a752285c4a6897fb2f0490
OscarLight <oscarli2716@gmail.com>
2018-02-01 02:06:34 -0500

    c model for transiXNOR

--- transiNXOR_modeling/c_model/device_model.c
+++ transiNXOR_modeling/c_model/device_model.c
@@ -293,7 +293,7 @@ float device_model(
 	// Layer 1
   fc(16, 1, 16, sig_fc_layer_1_w, sig_fc_layer_0_b, sig_fc_layer_1_b);
 	matmul(16, 1, 16, tanh_fc_layer_1_w, tanh_temp0, tanh_temp1);
-	fc(16, 1, 16, inter_embed_layer_1_w, tanh_temp0, inter_embed_layer_1_b);
+	fc(16, 1, 16, inter_embed_layer_1_w, tanh_temp1, inter_embed_layer_1_b);
   print_array(inter_embed_layer_1_b, 16);
 	add(16, inter_embed_layer_1_b, sig_fc_layer_1_b);;
   sig_act(sig_fc_layer_1_b, 16);
