4f27e2e421d72454937f160c63f6ac03c5db27df
OscarLight <oscarli2716@gmail.com>
2018-02-01 02:21:50 -0500

    c model for transiXNOR

---
+++ transiNXOR_modeling/c_model/device_model.h
@@ -0,0 +1,9 @@
+#ifndef _DEVICE_MODEL_H_   /* Include guard */
+#define _DEVICE_MODEL_H_
+
+float device_model(
+	const float vtg, const float vbg, 
+  	const float vds, const float w
+);
+
+#endif // _DEVICE_MODEL_H_
