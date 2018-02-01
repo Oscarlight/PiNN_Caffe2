8fd1b46310b4e3cddc00b6971cb778e851865173
OscarLight <oscarli2716@gmail.com>
2018-02-01 08:30:58 -0500

    c model for transiXNOR

--- transiNXOR_modeling/c_model/device_model.h
+++ transiNXOR_modeling/c_model/device_model.h
@@ -1,9 +1,13 @@
 #ifndef _DEVICE_MODEL_H_   /* Include guard */
 #define _DEVICE_MODEL_H_
 
+// INPUTS: voltage in V, w in m, 
+// OUTPUT: current in A
 float device_model(
-	const float vtg, const float vbg, 
-  	const float vds, const float w
+	const float vtg, 
+	const float vbg, 
+  	const float vds, 
+  	const float w
 );
 
 #endif // _DEVICE_MODEL_H_
