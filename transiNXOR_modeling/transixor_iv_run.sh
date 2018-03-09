edf4843a6b7b936b432380bf450ad8c582495f93
OscarLight <oscarli2716@gmail.com>
2018-03-09 07:24:14 -0800

    training for extended voltage

--- transiNXOR_modeling/transixor_iv_run.sh
+++ transiNXOR_modeling/transixor_iv_run.sh
@@ -11,4 +11,5 @@
 ## extend V to -0.1 to 0.3 V
 # python transixor_trainer.py transiXOR_Models/bise_ext_h216_0 -hidden 16 -layer 2 -mls 500 -batchsize 2048 -lossfunct scaled_l2
 # python transixor_trainer.py transiXOR_Models/bise_ext_h216_1 -hidden 16 -layer 2 -mls 50 -batchsize 2048 -lossfunct scaled_l1
-python transixor_trainer.py transiXOR_Models/bise_ext_h232_0 -hidden 32 -layer 2 -mls 5000 -batchsize 2048 -lossfunct scaled_l2
+# python transixor_trainer.py transiXOR_Models/bise_ext_h232_0 -hidden 32 -layer 2 -mls 5000 -batchsize 2048 -lossfunct scaled_l2
+python transixor_trainer.py transiXOR_Models/bise_ext_h232_1 -hidden 32 -layer 2 -mls 500 -batchsize 2048 -lossfunct scaled_l1
