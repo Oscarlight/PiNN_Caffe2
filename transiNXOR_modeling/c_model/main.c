1bca1342cf131bbf287807a5b802fc76b2d4bb8d
OscarLight <oscarli2716@gmail.com>
2018-03-10 12:02:37 -0800

    test c-model

--- transiNXOR_modeling/c_model/main.c
+++ transiNXOR_modeling/c_model/main.c
@@ -6,12 +6,12 @@ int main(int argc, char** argv) {
     double vtg = 0.2;
     double vtb = 0.2;
     double vds = 0.2;
-	double id = device_model(vtg, vtb, vds, 1);
-	printf("%f\n", id);
-	id = device_model(0.0, 0.0, 0.2, 1);
-	printf("%f\n", id);
-	id = device_model(0.1, 0.0, 0.2, 1);
-	printf("%f\n", id);
-	id = device_model(0.0, 0.1, 0.2, 1);
-	printf("%f\n", id);
+	// double id = device_model(vtg, vtb, vds, 1);
+	// printf("%f\n", id);
+	// id = device_model(0.0, 0.0, 0.2, 1);
+	// printf("%f\n", id);
+	// id = device_model(0.1, 0.0, 0.2, 1);
+	// printf("%f\n", id);
+	// id = device_model(0.0, 0.1, 0.2, 1);
+	// printf("%f\n", id);
 }
