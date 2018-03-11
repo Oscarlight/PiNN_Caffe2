834d50500e498037cfc95ab4388cf3a4d0dd1f2c
ota2 <ota2@cornell.edu>
2018-03-10 19:42:16 -0500

    Revert "Limit voltage range and add native matlab implementation"
    This reverts commit 42f0266c20a5f33e3714596763cc65c2b7c06447.

--- transiNXOR_modeling/c_model/test_model.m
+++ transiNXOR_modeling/c_model/test_model.m
@@ -1,17 +1,8 @@
-params = load('params.mat');
-
 N = 1000;
 res = zeros(1, N);
-res_mat = zeros(1, N);
-
 vds = linspace(0, 0.2, N);
 for i = 1:N
     res(i) = wrappedModel(0.2, 0.0, vds(i), 1);
-    res_mat(i) = ids(0.2, 0.0, vds(i), 1, params);
 end
 
 plot(vds, res, 'LineWidth', 1.5);
-hold on
-plot(vds, res_mat, 'LineWidth', 1.5);
-legend({'c_model', 'matlab'});
-hold off
