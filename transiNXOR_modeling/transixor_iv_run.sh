64433d19d4e54c273b3bef9b11d3bfa34443b17d
OscarLight <oscarli2716@gmail.com>
2018-05-13 18:03:06 -0700

    better predictor

--- transiNXOR_modeling/transixor_iv_run.sh
+++ transiNXOR_modeling/transixor_iv_run.sh
@@ -1,32 +1,19 @@
-# python transixor_trainer.py transiXOR_Models/bise_h18_0 -hidden 8 -layer 1
-# python transixor_trainer.py transiXOR_Models/bise_h28_0 -hidden 8 -layer 2
-# python transixor_trainer.py transiXOR_Models/bise_h116_0 -hidden 16 -layer 1
-# python transixor_trainer.py transiXOR_Models/bise_h216_0 -hidden 16 -layer 2
-
-# python transixor_trainer.py transiXOR_Models/bise_h18_1 -hidden 8 -layer 1 -mls 500
-# python transixor_trainer.py transiXOR_Models/bise_h28_1 -hidden 8 -layer 2 -mls 500
-# python transixor_trainer.py transiXOR_Models/bise_h116_1 -hidden 16 -layer 1 -mls 500
-# python transixor_trainer.py transiXOR_Models/bise_h216_1 -hidden 16 -layer 2 -mls 500
-
-## extend V to -0.1 to 0.3 V
-# python transixor_trainer.py transiXOR_Models/bise_ext_h216_0 -hidden 16 -layer 2 -mls 500 -batchsize 2048 -lossfunct scaled_l2
-# python transixor_trainer.py transiXOR_Models/bise_ext_h216_1 -hidden 16 -layer 2 -mls 50 -batchsize 2048 -lossfunct scaled_l1
-# python transixor_trainer.py transiXOR_Models/bise_ext_h232_0 -hidden 32 -layer 2 -mls 5000 -batchsize 2048 -lossfunct scaled_l2
-# python transixor_trainer.py transiXOR_Models/bise_ext_h232_1 -hidden 32 -layer 2 -mls 500 -batchsize 2048 -lossfunct scaled_l1
 ## current best one (note: the definition of hidden layer changed):
-# python transixor_trainer.py transiXOR_Models/bise_ext_sym_h264_0 -hidden 64 -layer 2 -mls 500 -batchsize 2048 -lossfunct scaled_l1 -epoch 500000
-# python transixor_trainer.py transiXOR_Models/bise_ext_sym_h2_128_0 -hidden 128 128 -mls 500 -batchsize 2048 -lossfunct scaled_l1 -epoch 5000000
-# python transixor_trainer.py transiXOR_Models/bise_ext_sym_h2_128_64_0 -hidden 128 64 -mls 500 -batchsize 2048 -lossfunct scaled_l1 -epoch 5000000
-# python transixor_trainer.py transiXOR_Models/bise_ext_sym_h2_64_0 -lr 0.05 -hidden 64 64 -mls 500 -batchsize 2048 -lossfunct scaled_l1 -epoch 5000000
+# python transixor_trainer.py transiXOR_Models/bise_ext_sym_h264_0 \
+# -hidden 64 -layer 2 -mls 500 -batchsize 2048 -lossfunct scaled_l1 -epoch 500000
 
 ## add neg grad penalty
 python transixor_trainer.py transiXOR_Models/bise_ext_sym_h264_neggrad_1 \
-	-hidden 64 64 \
-	-mls 500 \
-	-batchsize 2048 \
-	-lossfunct scaled_l1 \
-	-epoch 10000 \
-	-neg_grad_mag 10 \
-	-report 1000
+	-hidden 64 64 -mls 500 -batchsize 2048 -lossfunct scaled_l1 \
+	-epoch 500000 -neg_grad_mag 1 -report 1000
+
+python transixor_trainer.py transiXOR_Models/bise_ext_sym_h264_neggrad_1 \
+	-hidden 64 64 -mls 500 -batchsize 2048 -lossfunct scaled_l1 \
+	-epoch 500000 -neg_grad_mag 10 -report 1000
+
+python transixor_trainer.py transiXOR_Models/bise_ext_sym_h264_neggrad_1 \
+	-hidden 64 64 -mls 500 -batchsize 2048 -lossfunct scaled_l1 \
+	-epoch 500000 -neg_grad_mag 100 -report 1000
+
 
 
--- transiNXOR_modeling/transixor_predictor.py
+++ transiNXOR_modeling/transixor_predictor.py
@@ -1,4 +1,4 @@
-import sys
+import sys, os
 sys.path.append('../')
 import numpy as np
 from itertools import product
@@ -6,63 +6,70 @@ from pinn_api import predict_ids_grads, predict_ids
 import matplotlib.pyplot as plt
 import glob
 
-
+## ------------  Input  ---------------
+VDS = None
+VTG = 0.05
+VBG = 0.05
 ## ------------  True data  ---------------
 ids_file = glob.glob('./transiXOR_data/current_D9.npy')
-# ids_file = glob.glob('./transiXOR_data/*_id_*.npy')
-# vds, vbg, vtg, id
-ids_data = np.load(ids_file[0])
-print(ids_data.shape)
+data_true = np.load(ids_file[0])
+## ------------  Helper Funs  ---------------
+def ids(data, vds=None, vbg=None, vtg=None):
+	# vds, vbg, vtg, id
+	# assume v from -0.1 to 0.3, total 41 points
+	vid = lambda v : int(round((v + 0.1)/0.01))
+	if vds and vbg:
+		return data[vid(vds), vid(vbg), :]
+	if vtg and vbg:
+		return data[:, vid(vbg), vid(vtg)]
+	else:
+		raise Exception('Not Supported')
 
+def plot(data_pred, data_true, vds=None, vbg=None, vtg=None):
+	plt.plot(ids(data_pred, vds=vds, vbg=vbg, vtg=vtg), 'r')
+	plt.plot(ids(data_true, vds=vds, vbg=vbg, vtg=vtg), 'b')
+	plt.show()
+	plt.semilogy(ids(data_pred, vds=vds, vbg=vbg, vtg=vtg), 'r')
+	plt.semilogy(ids(data_true, vds=vds, vbg=vbg, vtg=vtg), 'b')
+	plt.show()
 ## ------------  Prediction ---------------
-vds = np.linspace(-0.1, 0.3, 41)
-vbg = np.linspace(0.1, 0.1, 1)
-vtg = np.linspace(0.2, 0.2, 1)
-# vds = np.linspace(0.0, 0.0, 1)
-# vbg = np.linspace(0.1, 0.1, 1)
-# vtg = np.linspace(-0.1, 0.3, 41)
-
-iter_lst = list(product(vds, vbg, vtg))
-vds_pred = np.expand_dims(np.array([e[0] for e in iter_lst], dtype=np.float32), axis=1)
-vbg_pred = np.array([e[1] for e in iter_lst], dtype=np.float32)
-vtg_pred = np.array([e[2] for e in iter_lst], dtype=np.float32)
-vg_pred = np.column_stack((vtg_pred, vbg_pred))
-
-vg_pred = np.sum(vg_pred, axis=1, keepdims=True)
-
-# vg_pred = np.sum(vg_pred, axis=1, keepdims=True)
-
-model_path = './transiXOR_Models/'
+pred_data_path = 'pred_data/'
 # model_name = 'bise_ext_sym_h264_0'
 model_name = 'bise_ext_sym_h264_neggrad_1'
-
-## If trained with adjoint builder
-ids_pred, _, _ = predict_ids_grads(
-	model_path + model_name, vg_pred, vds_pred)
-## If trained with origin builder
-# ids_pred = predict_ids(
-# 	model_path + model_name, vg_pred, vds_pred)
-
-ids_true = ids_data[:, 30, 20]
-vds_true = np.linspace(-0.1, 0.3, 41)
-plt.plot(vds, ids_pred, 'r')
-plt.plot(vds_true, ids_true)
-plt.show()
-plt.semilogy(vds, np.abs(ids_pred), 'r') 
-plt.semilogy(vds_true, np.abs(ids_true))
-plt.show()
-
-# ids_true = ids_data[10, 20, :]
-# vtg_true = np.linspace(-0.1, 0.3, 41)
-# plt.plot(vtg, ids_pred, 'r')
-# plt.plot(vtg_true, ids_true)
-# plt.show()
-# plt.semilogy(vtg, np.abs(ids_pred), 'r') 
-# plt.semilogy(vtg_true, np.abs(ids_true))
-# plt.show()
+if not os.path.isfile(pred_data_path + model_name + '.npy'):
+	print('Computing all data...')
+	vds = np.linspace(-0.1, 0.3, 41)
+	vbg = np.linspace(-0.1, 0.3, 41)
+	vtg = np.linspace(-0.1, 0.3, 41)
+	iter_lst = list(product(vds, vbg, vtg))
+	vds_pred = np.expand_dims(np.array([e[0] for e in iter_lst], dtype=np.float32), axis=1)
+	vbg_pred = np.array([e[1] for e in iter_lst], dtype=np.float32)
+	vtg_pred = np.array([e[2] for e in iter_lst], dtype=np.float32)
+	vg_pred = np.column_stack((vtg_pred, vbg_pred))
+	vg_pred = np.sum(vg_pred, axis=1, keepdims=True) # model use symmetry vtg vbg
+	model_path = './transiXOR_Models/'
+	## If trained with adjoint builder
+	data_pred_flat, _, _ = predict_ids_grads(
+		model_path + model_name, vg_pred, vds_pred)
+	data_pred = np.zeros((41, 41, 41))
+	idx = 0
+	for i in range(41):
+		for j in range(41):
+			for k in range(41):
+				data_pred[i, j, k] = data_pred_flat[idx]
+				idx += 1
+	## If trained with origin builder
+	# ids_pred = predict_ids(
+	# 	model_path + model_name, vg_pred, vds_pred)
+	np.save(pred_data_path + model_name + '.npy', data_pred) 
+	plot(data_pred, data_true, vds=VDS, vbg=VBG, vtg=VTG)
+else:
+	print('Reading from pre-computed data...')
+	data_pred = np.load(pred_data_path + model_name + '.npy')
+	print(data_pred.shape)
+	plot(data_pred, data_true, vds=VDS, vbg=VBG, vtg=VTG)
 
 ## Point test
-
 # ids_pred = predict_ids(
 # 	'./transiXOR_Models/bise_ext_sym_h264_0',
 # 	np.array([0.2+0.2]), np.array([0.2]))
