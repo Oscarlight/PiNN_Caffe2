cd778e23966fc4e9361536f4965d6405c29c4322
OscarLight <oscarli2716@gmail.com>
2018-05-22 23:34:10 -0700

    enable init model from previous param

--- transiNXOR_modeling/transixor_iv_run.sh
+++ transiNXOR_modeling/transixor_iv_run.sh
@@ -5,7 +5,7 @@
 ## add neg grad penalty
 python transixor_trainer.py transiXOR_Models/bise_ext_sym_h264_neggrad_0 \
 	-hidden 64 64 -mls 500 -batchsize 2048 -lossfunct scaled_l1 \
-	-epoch 5000 -neg_grad_mag 1 -report 1 -lr 0.01
+	-epoch 5000 -neg_grad_mag 1 -report 1000 -lr 0.01
 
 python transixor_trainer.py transiXOR_Models/bise_ext_sym_h264_neggrad_1 \
 	-hidden 64 64 -mls 500 -batchsize 2048 -lossfunct scaled_l1 \
